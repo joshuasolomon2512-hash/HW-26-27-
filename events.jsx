c8dd0a685db45e2c43522858bcd109ccc6d2cc31
joshua solomon <joshuasolomon2512@gmail.com>
2026-08-13 10:31:59 +0530

    Update events.jsx

--- events.jsx
+++ events.jsx
@@ -66,7 +66,7 @@ export const events = [
     phases: [ "HTML/CSS/JS", "1 Hr Limit"],
     
     // Manual status/phase overrides (optional)
-    isLive: false,
+    isLive: true,
     status: "",
     phase: "",
     winners: []
